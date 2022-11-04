1d54ec0768bf806934dd25476f98b83b420e5ee3
Cameron Miner <cameronsminer@gmail.com>
2022-11-04 12:01:16 -0600

    Added automated tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,30 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+
+// Two extra tests
+
+test('Draw button displays the div with id = choices', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    await draw.click()
+    await driver.sleep(5000)
+
+    const choices = await driver.findElement(By.id('choices'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Add to Duo button displayed the div with id = player-duo', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    await draw.click()
+    await driver.sleep(5000)
+
+    const add = await driver.findElement(By.xpath('//button[@class="bot-btn"]'))
+    await add.click()
+    await driver.sleep(5000)
+
+    const playerDuo = await driver.findElement(By.id('player-duo'))
+    const nextDisplayed = await playerDuo.isDisplayed()
+    expect (nextDisplayed).toBe(true)
+})
